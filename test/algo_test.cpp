41cf4ca4be157ca2bd46d69eb5f32fee170d47dc
Ahmed Kabir <saminyeaserlight@gmail.com>
2024-01-19 01:06:43 +0000

    data syncing

--- test/algo_test.cpp
+++ test/algo_test.cpp
@@ -168,9 +168,8 @@ void SPTAG_BKT(std::string base_path, std::string query_path, std::string ground
     //std::cout << "Time cost: " << builder->GetBuildTime().count() << std::endl;
 }
 
-void HCNNG(std::string base_path, std::string query_path, std::string ground_path, unsigned K = 20) {
-    std::string graph = "hcnng.graph";
-
+void HCNNG(std::string base_path, std::string query_path, std::string ground_path, unsigned K = 20, std::string graph = "hcnng.graph") {
+    cout<<"Graph name: "<<graph<<endl;
     weavess::Parameters parameters;
     parameters.set<unsigned>("minsize_cl", 100);
     parameters.set<unsigned>("num_cl", 20);
@@ -192,13 +191,357 @@ void HCNNG(std::string base_path, std::string query_path, std::string ground_pat
     //std::cout << "Time cost: " << builder->GetBuildTime().count() << std::endl;
 }
 
-int main(int argc, char** argv) {
-	
+int testSift1m() {
+    freopen("sift1m.txt","a+",stdout);
     std::string base_path = "sift/sift_base.fvecs";
     std::string query_path = "sift/sift_query.fvecs";
     std::string ground_path = "sift/sift_groundtruth.ivecs";
+    std::string graph = "sift1mhcnng.graph";
+    int K = 100;
 
-    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, 100);
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testGist1m() {
+    freopen("gist1m.txt","a+",stdout);
+    std::string base_path = "gist/gist_base.fvecs";
+    std::string query_path = "gist/gist_query.fvecs";
+    std::string ground_path = "gist/gist_groundtruth.ivecs";
+    std::string graph = "gist1mhcnng.graph";
+    int K = 100;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testGlove() {
+    freopen("glove.txt","a+",stdout);
+    std::string base_path = "glove/base.fvecs";
+    std::string query_path = "glove/query.fvecs";
+    std::string ground_path = "glove/groundtruth.ivecs";
+    std::string graph = "glovehcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testimageNet() {
+    freopen("imageNet.txt","a+",stdout);
+    std::string base_path = "imageNet/base.fvecs";
+    std::string query_path = "imageNet/query.fvecs";
+    std::string ground_path = "imageNet/groundtruth.ivecs";
+    std::string graph = "imageNethcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testnotre() {
+    freopen("notre.txt","a+",stdout);
+    std::string base_path = "notre/base.fvecs";
+    std::string query_path = "notre/query.fvecs";
+    std::string ground_path = "notre/groundtruth.ivecs";
+    std::string graph = "notrehcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testukbench() {
+    freopen("ukbench.txt","a+",stdout);
+    std::string base_path = "ukbench/base.fvecs";
+    std::string query_path = "ukbench/query.fvecs";
+    std::string ground_path = "ukbench/groundtruth.ivecs";
+    std::string graph = "ukbenchhcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testCrawl() {
+    freopen("crawl.txt","a+",stdout);
+    std::string base_path = "crawl/base.fvecs";
+    std::string query_path = "crawl/query.fvecs";
+    std::string ground_path = "crawl/groundtruth.ivecs";
+    std::string graph = "crawlhcnng.graph";
+    int K = 100;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testAudio() {
+    freopen("audio.txt","a+",stdout);
+    std::string base_path = "audio/audio_base.fvecs";
+    std::string query_path = "audio/audio_query.fvecs";
+    std::string ground_path = "audio/audio_groundtruth.ivecs";
+    std::string graph = "audiohcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testCifar() {
+    freopen("cifar.txt","a+",stdout);
+    std::string base_path = "cifar/base.fvecs";
+    std::string query_path = "cifar/query.fvecs";
+    std::string ground_path = "cifar/groundtruth.ivecs";
+    std::string graph = "cifarhcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testEnron() {
+    freopen("enron.txt","a+",stdout);
+    std::string base_path = "enron/base.fvecs";
+    std::string query_path = "enron/query.fvecs";
+    std::string ground_path = "enron/groundtruth.ivecs";
+    std::string graph = "enronhcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+
+int testmillionSong() {
+    freopen("millionSong.txt","a+",stdout);
+    std::string base_path = "millionSong/base.fvecs";
+    std::string query_path = "millionSong/query.fvecs";
+    std::string ground_path = "millionSong/groundtruth.ivecs";
+    std::string graph = "millionSonghcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testMnist() {
+    freopen("MNIST.txt","a+",stdout);
+    std::string base_path = "MNIST/base.fvecs";
+    std::string query_path = "MNIST/query.fvecs";
+    std::string ground_path = "MNIST/groundtruth.ivecs";
+    std::string graph = "MNISThcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;
+}
+
+
+int testNuswide() {
+    freopen("nuswide.txt","a+",stdout);
+    std::string base_path = "nuswide/base.fvecs";
+    std::string query_path = "nuswide/query.fvecs";
+    std::string ground_path = "nuswide/groundtruth.ivecs";
+    std::string graph = "nuswidehcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+
+int testSun() {
+    freopen("sun.txt","a+",stdout);
+    std::string base_path = "sun/base.fvecs";
+    std::string query_path = "sun/query.fvecs";
+    std::string ground_path = "sun/groundtruth.ivecs";
+    std::string graph = "sunhcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testDeep() {
+    freopen("deep.txt","a+",stdout);
+    std::string base_path = "deep/base.fvecs";
+    std::string query_path = "deep/query.fvecs";
+    std::string ground_path = "deep/groundtruth.ivecs";
+    std::string graph = "deephcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+
+int testTrevi() {
+    freopen("trevi.txt","a+",stdout);
+    std::string base_path = "trevi/base.fvecs";
+    std::string query_path = "trevi/query.fvecs";
+    std::string ground_path = "trevi/groundtruth.ivecs";
+    std::string graph = "trevihcnng.graph";
+    int K = 20;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+
+int testUqv() {
+    freopen("uqv.txt","a+",stdout);
+    std::string base_path = "uqv/base.fvecs";
+    std::string query_path = "uqv/query.fvecs";
+    std::string ground_path = "uqv/groundtruth.ivecs";
+    std::string graph = "uqvhcnng.graph";
+    int K = 100;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+
+int testNyTimes() {
+    freopen("nytimes.txt","a+",stdout);
+    std::string base_path = "nytimes/base.fvecs";
+    std::string query_path = "nytimes/query.fvecs";
+    std::string ground_path = "nytimes/groundtruth.ivecs";
+    std::string graph = "nytimeshcnng.graph";
+    int K = 100;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+
+int testLastFm() {
+    freopen("lastfm.txt","a+",stdout);
+    std::string base_path = "lastfm/base.fvecs";
+    std::string query_path = "lastfm/query.fvecs";
+    std::string ground_path = "lastfm/groundtruth.ivecs";
+    std::string graph = "lastfmhcnng.graph";
+    int K = 100;
+
+    HCNNG(absolute_path + base_path, absolute_path + query_path, absolute_path + ground_path, K, graph);
+
+    return 0;  
+}
+
+int main(int argc, char* argv[]) {
+	
+    for (int i=0;i<argc;i++) {
+        if (strcmp(argv[i],"sift1m") == 0) {
+                cout<<"Sift1m test"<<endl;
+                testSift1m();
+        }
+        else if (strcmp(argv[i],"gist") == 0) {
+                cout<<"gist test"<<endl;
+                testGist1m();
+        }
+        else if (strcmp(argv[i],"glove") == 0) {
+                cout<<"glove test"<<endl;
+                testGlove();
+        }
+        else if (strcmp(argv[i],"imagenet") == 0) {
+                cout<<"imagenet test"<<endl;
+                testimageNet();
+        }
+        else if (strcmp(argv[i],"notre") == 0) {
+                cout<<"notre test"<<endl;
+                testnotre();
+        }
+        else if (strcmp(argv[i],"ukbench") == 0) {
+                cout<<"ukbench test"<<endl;
+                testukbench();
+        }
+        else if (strcmp(argv[i],"crawl") == 0) {
+                cout<<"crawl test"<<endl;
+                testCrawl();
+        }
+        else if (strcmp(argv[i],"audio") == 0) {
+                cout<<"audio test"<<endl;
+                testAudio();
+        }
+        else if (strcmp(argv[i],"cifer") == 0) {
+                cout<<"cifer test"<<endl;
+                testCifar();
+        }
+        else if (strcmp(argv[i],"enron") == 0) {
+                cout<<"enron test"<<endl;
+                testEnron();
+        }
+        else if (strcmp(argv[i],"millionSong") == 0) {
+                cout<<"millionSong test"<<endl;
+                testmillionSong();
+        }
+        else if (strcmp(argv[i],"MNIST") == 0) {
+                cout<<"MNIST test"<<endl;
+                testMnist();
+        }
+        else if (strcmp(argv[i],"nuswide") == 0) {
+                cout<<"nuswide test"<<endl;
+                testNuswide();
+        }
+        else if (strcmp(argv[i],"sun") == 0) {
+                cout<<"sun test"<<endl;
+                testSun();
+        }
+        else if (strcmp(argv[i],"deep") == 0) {
+                cout<<"deep test"<<endl;
+                testDeep();
+        }
+        else if (strcmp(argv[i],"trevi") == 0) {
+                cout<<"trevi test"<<endl;
+                testTrevi();
+        }
+        else if (strcmp(argv[i],"uqv") == 0) {
+                cout<<"uqv test"<<endl;
+                testUqv();
+        }
+        else if (strcmp(argv[i],"nytimes") == 0) {
+                cout<<"nytimes test"<<endl;
+                testNyTimes();
+        }
+        else if (strcmp(argv[i],"lastfm") == 0) {
+                cout<<"lastfm test"<<endl;
+                testLastFm();
+        }
+        
+    }
 
     return 0;
 }
